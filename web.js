448699e628a047d5fc54cf40940ea66e383168d9
Jeff Schomay <jschomay@gmail.com>
2015-10-04 17:19:23 -0600

    Add simple privacy policy url

--- web.js
+++ web.js
@@ -7,6 +7,10 @@ app.post('/', function (req, res, next) {
     next();
 });
 
+app.get('/privacy', function (req, res, next) {
+    res.send("This game is for the purpose of fun, and my privacy policy is simple.  I use your Facebook login to store your progress in the game.  Other than that, I use some forms of analytics to see where my marketing efforts are working or not, and I may show 3rd party adds and use a 3rd party payment processor for in app purchases.  I never sell any of your information to 3rd parties.");
+});
+
 app.use(express.static('./'));
 
 var port = process.env.PORT || 3000;
